5ae7b793e3e8ef3f73b519facc95394b7a9326a3
Shahrukh Omar <shahrukh.omar@infinity.co>
2019-04-12 20:23:40 +0100

    simple solution

--- js/app.js
+++ js/app.js
@@ -4,9 +4,38 @@
 
   //--------------- put your code below this line -------------
 
-  // input field for the calculator screen
-  var output = document.getElementById('output');
+  // output field for the calculator screen
+  let output = document.getElementById('output');
 
-  // calculator button
-  var bttn1 = document.querySelector('[name="bttn1"]');
+  Array.from(
+    document.querySelectorAll(".btn")).map(
+      (e) => e.addEventListener('click', ({ target: { value } }) => {
+
+        if (value === 'C') {
+          return output.value = '';
+        }
+
+        if (value === '=') {
+          let ops = output.value.split(/(\+|-|x)/);
+
+          let comp = (op, cb) => {
+            while (ops.indexOf(op) > -1) {
+              let i = ops.indexOf(op);
+              ops[i+1] = cb(parseInt(ops[i-1]), parseInt(ops[i+1]));
+              ops.splice(i-1, 2);
+            }
+          }
+
+          // BODMAS
+          comp('x', (a,b) => a * b);
+          comp('+', (a,b) => a + b);
+          comp('-', (a,b) => a - b);
+
+          return output.value += `=${ops[0]}`;
+        }
+
+        output.value += value;
+      }
+    )
+  );
 })();
