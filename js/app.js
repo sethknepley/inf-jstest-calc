a57379b8d5d9d56c10ed7535a685a4e3ae76a603
Shahrukh Omar <shahrukh.omar@infinity.co>
2019-04-12 16:33:05 +0100

    First commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// self executing main function
+(function() {
+  console.log('Welcome to the calculator app');
+
+  //--------------- put your code below this line -------------
+
+  // input field for the calculator screen
+  var output = document.getElementById('output');
+
+  // calculator button
+  var bttn1 = document.querySelector('[name="bttn1"]');
+})();
